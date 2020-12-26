c8caaa21e4552ad9a256ccd7b8abb238fa8bb88f
antsPls <zzzhafir75@gmail.com>
2020-12-26 20:01:54 +0100

    Implement server-based login and register.
    Remove most of the old code in User that used local authentication. Redo LoginController and RegisterController.
    Add HttpRequestHandler and HttpUtils to deal with any network related stuff. Modify some FXMLs to be able to reference the buttons and add some styling to the register view.

--- src/test/java/models/GeneralTest.java
+++ src/test/java/models/GeneralTest.java
@@ -1,36 +1,39 @@
 package models;
 
-import arbyte.models.CalEvent;
-import arbyte.models.Kalendar;
-import arbyte.models.User;
 
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
+import arbyte.helper.HttpRequestHandler;
+import arbyte.models.User;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+import java.util.concurrent.CompletableFuture;
 
 public class GeneralTest {
     public static void main(String[] args) {
-        Kalendar kalendar = new Kalendar();
-        CalEvent event2 = new CalEvent("benis1", dateTime("20211218T16:00"), dateTime("20211218T17:00"));
-        CalEvent event3 = new CalEvent("benis1", dateTime("20221218T16:00"), dateTime("20221218T17:00"));
-        kalendar.addEventToMonth(event2);
-        kalendar.addEventToMonth(event3);
+        try {
+            User user = new User("refreshtest@example.com", "password", "password");
 
-        System.out.println(kalendar.toJson());
-        kalendar = Kalendar.fromJson("{\"months\":[{\"monthYear\":\"12-2021\",\"events\":[{\"name\":\"benis1\",\"startTime\":\"2021-12-18T16:00:00+01:00\",\"endTime\":\"2021-12-18T17:00:00+01:00\"}]},{\"monthYear\":\"12-2022\",\"events\":[{\"name\":\"benis1\",\"startTime\":\"2022-12-18T16:00:00+01:00\",\"endTime\":\"2022-12-18T17:00:00+01:00\"}]}]}");
-        System.out.println(kalendar.toJson());
+            var responseFuture
+                    = HttpRequestHandler.postRequest("/login", user.toJson());
+            System.out.println("Getting resource");
+            long t1 = System.currentTimeMillis();
 
-        User user = new User("test@gmail.com", "123", "123");
-        System.out.println(user.toJson());
-    }
+            var response = responseFuture.get();
+            long t2 = System.currentTimeMillis();
 
-    private  static ZonedDateTime dateTime(String dateTime){
-        if(dateTime.length() != 14){
-            System.out.println("Error Wrong dateTime Format");
-            return ZonedDateTime.now();
+            System.out.println(response.statusCode());
+            System.out.println(response.body());
+            System.out.println(t2 - t1);
+
+            Gson gson = new GsonBuilder().create();
+            JsonObject jsonObject = gson.fromJson(response.body(), JsonObject.class);
+
+            System.out.println(jsonObject.get("message"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        String time = dateTime.substring(0, 4) + "-" + dateTime.substring(4, 6) + "-" + dateTime.substring(6, 8)
-                + "T" + dateTime.substring(9, 11) + ":" + dateTime.substring(12, 14) + ":00+01:00";
-        return ZonedDateTime.parse(time, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
 
     }
 }
