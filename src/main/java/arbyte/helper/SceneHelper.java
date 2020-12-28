08923a0a7ed62fac59ea67933b287e7b96f8be8c
AflahAmini <aflahamini@gmail.com>
2020-12-28 18:01:50 +0100

    Add MonthYear Label , previous and next Button to CalendarView.fxml Implement method to attach CalendarView.fxml to MainScene Add getParentFromFXML method Rewrite switchSceneResource Add CalendarButton.fxml

--- src/main/java/arbyte/helper/SceneHelper.java
+++ src/main/java/arbyte/helper/SceneHelper.java
@@ -4,6 +4,7 @@ package arbyte.helper;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 
@@ -17,39 +18,38 @@ public class SceneHelper {
     }
 
     public static void showLogInPage(){
-        SceneHelper.switchSceneResource("fxml/LoginPage.fxml", "Log In", 600, 400);
+        SceneHelper.switchSceneResource("fxml/LoginPage.fxml", 600, 400);
 
     }
 
     public static void showRegisterPage(){
-        SceneHelper.switchSceneResource("fxml/RegisterPage.fxml", "Register", 600, 400);
+        SceneHelper.switchSceneResource("fxml/RegisterPage.fxml",600, 400);
     }
 
     public static void showMainPage(){
-        SceneHelper.switchSceneResource("fxml/MainScene.fxml", "Main", 900, 600);
+        SceneHelper.switchSceneResource("fxml/MainScene.fxml", 900, 600);
     }
 
     public static void showAddEvent(){
-        SceneHelper.switchSceneResource("fxml/EventAdd.fxml", "Add Event", 600, 400);
+        SceneHelper.switchSceneResource("fxml/EventAdd.fxml", 600, 400);
     }
 
-
-    private static void switchSceneResource(String filename, String scenename, int height, int width){
-
-        try{
+    public static Parent getParentFromFXML(String filename) {
+        try {
             ResourceLoader loader = new ResourceLoader();
-            Parent source = FXMLLoader.load(loader.getURLFromResource(filename));
-            Scene scene = new Scene(source, height, width);
-            currentStage.setScene(scene);
-            currentStage.show();
+            System.out.println(loader.getURLFromResource(filename).toString());
+            return FXMLLoader.load(loader.getURLFromResource(filename));
         }
         catch(Exception e){
-            System.out.println(scenename +" view failed");
+            System.out.println(filename + "view failed");
             e.printStackTrace();
         }
-
+        return new BorderPane();
     }
 
-
-
+    private static void switchSceneResource(String filename,  int height, int width){
+            Scene scene = new Scene(getParentFromFXML(filename), height, width);
+            currentStage.setScene(scene);
+            currentStage.show();
+    }
 }
