0609ee9ce6a0a8dd315b9bc81d4c635cbf19ac97
AflahAmini <aflahamini@gmail.com>
2020-12-31 14:14:36 +0100

    Rework on addButtonToCalendarView method

--- src/main/java/arbyte/controllers/CalendarViewController.java
+++ src/main/java/arbyte/controllers/CalendarViewController.java
@@ -8,6 +8,8 @@ import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.Calendar;
 
@@ -53,18 +55,26 @@ public class CalendarViewController {
         gridCalendar.getChildren().clear();
 
         YearMonth yearMonthObject =  YearMonth.of(year, month); //to get number of days in a month
+        LocalDate firstOfMonth = yearMonthObject.atDay( 1 );
+
         int counter = 0;
+        int day = firstOfMonth.getDayOfWeek().getValue();
+        int startingX = day ==7 ? 0 : day;
+        int incrementX = startingX;
+        int incrementY = 0;
 
-        outerloop:
-        for(int j = 0; j < gridCalendar.getRowConstraints().size(); j++){
-            for (int i = 0; i < gridCalendar.getColumnConstraints().size(); i++) {
-                Parent parent = SceneHelper.getParentFromFXML("fxml/CalendarButton.fxml");
-                gridCalendar.add(parent, i, j);
+
+        while(counter < yearMonthObject.getMonth().maxLength()){
+            Parent parent = SceneHelper.getParentFromFXML("fxml/CalendarButton.fxml");
+            System.out.println(startingX + " " + counter + " " + yearMonthObject.getMonth().maxLength());
+            if(incrementX < gridCalendar.getColumnConstraints().size()){
+                gridCalendar.add(parent, incrementX, incrementY);
+                incrementX++;
                 counter++;
-
-                if(counter >= yearMonthObject.getMonth().maxLength()){
-                    break outerloop;
-                }
+            }
+            else{
+                incrementX = 0;
+                incrementY++;
             }
         }
     }
